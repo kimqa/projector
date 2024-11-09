cf6a46f02aef566786ff9e5b82642b69aae19025
unknown <kimskad@gmail.com>
2024-11-09 23:06:36 +0200

    math operation

---
+++ HW1/HW1.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let number1 = 2;
+let number2 = 25;
+
+// Addition
+let sum;
+sum = number2+number1;
+console.log("number2 + number1 = " + sum);
+
+// Subtraction
+let minus = number2 - number1;
+console.log("number2 - number1 = " + minus);
+
+// Multiplication
+let multiplication = number1 * number2;
+console.log("number1 * number2 = " + multiplication)
+
+// Division
+let div = number2/number1;
+console.log("number2/number1 = " + div);
+
+// Exponentiation
+let exp = number2 ** number1;
+console.log("number2 ** number1 = " + exp);
+
+// Square root
+let squareRoot = number2 ** 0.5;
+console.log("Square root from number2 = " + squareRoot);
+

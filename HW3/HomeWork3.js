9d037b7d2b89301f5a8b23b5b41df709e49f60fb
Apple <kimskad@gmail.com>
2024-11-17 02:38:53 +0200

    t2

--- HW3/HomeWork3.js
+++ HW3/HomeWork3.js
@@ -17,3 +17,23 @@ function recursiveOddSumTo(number) {
 
 console.log(recursiveOddSumTo(1)) // 1
 console.log(recursiveOddSumTo(10)) // 25
+
+/*
+2. Задача про ітерацію
+Напишіть функцію яка ітеративно (в циклі) буде знаходити суму всіх непарних додатніх чисел до якогось числа.
+Приклад:
+ */
+function iterativeOddSumTo(number) {
+// тут ваш код
+    let sum = 0;
+    for (let i = 1; i <= number; ++i) {
+        if(i % 2 === 1) {
+            sum = sum + i;
+        } else {}
+    }
+    return sum;
+
+};
+
+console.log(iterativeOddSumTo(1)) // 1
+console.log(iterativeOddSumTo(10)) // 25

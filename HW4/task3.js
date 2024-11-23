40c6cf9c275da0f093f31f3478265fe73709a58b
unknown <kimskad@gmail.com>
2024-11-23 22:25:38 +0200

    task 3

---
+++ HW4/task3.js
@@ -0,0 +1,17 @@
+/*
+3. Задача на розворот числа:
+ */
+const currentMaxValue = 4589;
+let reverseMaxValue;
+
+// тут ваш код...
+reverseMaxValue='';
+let reverse = String(currentMaxValue);
+
+for (let i = reverse.length-1; i >= 0; i--) {
+    reverseMaxValue +=reverse[i];
+}
+reverseMaxValue = Number(reverseMaxValue);
+
+console.log(reverseMaxValue); // 9854
+console.log(typeof reverseMaxValue); // 'number'

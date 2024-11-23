3a91203f4d30b144e75d7229c4d9d2b52d482e16
unknown <kimskad@gmail.com>
2024-11-23 20:37:27 +0200

    task 1

--- HW4/task2.js
+++ HW4/task2.js
@@ -23,3 +23,54 @@ filteredNames = quserNames.map((name, index) => {
     }
 )
 console.log(filteredNames); // ['Емма', 'Юстин', 'Ілля', 'Яна', 'Антон', 'Олена']
+
+
+
+console.log(11111111111)
+
+
+
+// userNames.forEach(userNames => {
+// console.log(userNames);
+// })
+// console.log(userNames);
+console.log(typeof initials);
+console.log(initials); // ["Г.П.А.", "П.О.І.", "Р.А.О."]
+
+/*
+2. Задача на фільтрування масиву
+
+Реалізуйте фільтрування імен які починаються з голосної двома способами:
+
+через умовну конструкцію
+через вбудований метод масивів
+
+Рішення має працювати незалежно від конкретних значень в масиві імен
+ */
+const userNames_ = ['Петро', 'Емма', 'Юстин', 'Ілля', 'Марта', 'Яна', 'Василь', 'Антон', 'Олена'];
+let filteredNames;
+
+// тут ваш код...
+
+console.log(filteredNames); // ['Емма', 'Юстин', 'Ілля', 'Яна', 'Антон', 'Олена']
+
+/*
+3. Задача на розворот числа:
+ */
+const currentMaxValue = 4589;
+let reverseMaxValue;
+
+// тут ваш код...
+
+// console.log(reverseMaxValue); // 9854
+// console.log(typeof reverseMaxValue); // 'number'
+
+/*
+4. Задача на знаходження добутку масиву чисел з невідомою глибиною вкладеності:
+ */
+const resultsArray = [1, 2, [3, [4]]];
+let productOfArray;
+
+// тут ваш код...
+
+console.log(productOfArray); // 24

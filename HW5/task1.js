90516f8ad5c2132477fed4beb552f8c66d9dbea1
unknown <kimskad@gmail.com>
2024-11-24 01:04:19 +0200

    task 1

---
+++ HW5/task1.js
@@ -0,0 +1,46 @@
+/*
+1. Задача про обчислення різниці часу
+
+Напишіть функцію яка буде буде приймати 3 параметри
+
+— початкову дату (string)
+
+— кінцеву дату (string)
+
+— розмірність ('days',  'hours', 'minutes', seconds)
+
+Функція повертатиме часовий період між цими датами згідно розмірності.
+
+Також вкажіть значення по замовчуванню для всіх цих параметрів (на ваш вибір).
+
+Функція має коректно працювати навіть якщо початкова дата пізніше ніж кінцева дата.
+
+Приклади:
+ */
+function durationBetweenDates(start, end, size) {
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+    let dif = Math.abs(startDate - endDate)
+    switch (size) {
+        case 'seconds':
+            console.log(`${dif/1000} ${size}`);
+            break;
+        case 'minutes':
+            console.log(`${dif/1000/60} ${size}`);
+            break;
+        case 'hours':
+            console.log(`${dif/1000/60/60} ${size}`);
+            break;
+        case 'days':
+            console.log(`${dif/1000/60/60/24} ${size}`);
+            break;
+        default:
+            console.log(`${dif} ${size}`);
+
+
+    }
+
+
+}
+durationBetweenDates('02 Aug 1985', '03 Aug 1985', 'seconds')  // поверне '86400 seconds'
+durationBetweenDates('31 Jan 2022', '03 Feb 2021', 'days')  // поверне '362 days'

9ad9a450136b79c65d31a5b71df28b753a39a421
unknown <kimskad@gmail.com>
2024-12-17 20:42:15 +0200

    js14 workshop

---
+++ JS14/index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+
+const taskInput = document.querySelector('.task-input');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const form = document.querySelector('.create-task-form');
+console.log(form)
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log(event)
+    if (taskInput.value.trim()==='') {
+        return;
+    }
+    createSingleTaskElement(taskInput.value);
+    storageTaskInLocalStorage(taskInput.value);
+    taskInput.value = '';
+})
+
+
+function createSingleTaskElement(taskInput) {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(taskInput));
+    taskList.appendChild(li)
+}
+
+function storageTaskInLocalStorage(task) {
+    const tasks = localStorage.getItem('tasks') !== null ? JSON.parse(localStorage.getItem('tasks')) : [];
+    tasks.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const tasks = localStorage.getItem('tasks') !== null // тернальный оператор
+        ? JSON.parse(localStorage.getItem('tasks'))
+        : [];
+
+    tasks.forEach((task) => {
+        createSingleTaskElement(task);
+    })
+})
